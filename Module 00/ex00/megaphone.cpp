7723dba509a1b03bd013738980ee1938847af41a
Boldizsar Szabo <szboldisz@gmail.com>
2024-02-17 11:19:19 +0100

    Modify ex00

--- Module 00/ex00/megaphone.cpp
+++ Module 00/ex00/megaphone.cpp
@@ -1,31 +1,29 @@
-#include <iostream>
-#include <cctype>
-#include <string>
+#include <iostream> // cout, endl
+#include <string> // string
+#include <cctype> // toupper
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::toupper;
 
 // convert string 'str' to upper case and return it
-std::string	str_toupper(std::string *str)
+string	str_toupper(string str)
 {
-	std::string	upper_str = *str;
-
-	for (int i = 0; upper_str[i]; i++)
-		upper_str[i] = std::toupper(upper_str[i]);
-	return (upper_str);
+	for (int i = 0; str[i]; i++)
+		str[i] = toupper(str[i]);
+	return (str);
 }
 
 int	main(int argc, char *argv[])
 {
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		for (int i = 1; i < argc; i++)
-		{
-
-			std::string str = argv[i];
-			str = str_toupper(&str);
-			std::cout << str;
-		}
+			cout << str_toupper(argv[i]);
 	}
-	std::cout << std::endl;
+	cout << endl;
 	return (0);
 }
