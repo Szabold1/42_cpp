59a23649df93efd28ba908159bac9955846fcc86
Boldizsar Szabo <szboldisz@gmail.com>
2024-02-20 10:48:05 +0100

    Add ex00 and ex01

---
+++ module00/ex01/src/Contact.cpp
@@ -0,0 +1,45 @@
+#include "../include/Contact.hpp"
+
+using std::string;
+
+// Setter methods
+void	Contact::set_first_name(string str) {
+	this->first_name = str;
+}
+
+void	Contact::set_last_name(string str) {
+	this->last_name = str;
+}
+
+void	Contact::set_nickname(string str) {
+	this->nickname = str;
+}
+
+void	Contact::set_phone_number(string str) {
+	this->phone_number = str;
+}
+
+void	Contact::set_darkest_secret(string str) {
+	this->darkest_secret = str;
+}
+
+// Getter methods
+string	Contact::get_first_name(void) {
+	return (this->first_name);
+}
+
+string	Contact::get_last_name(void) {
+	return (this->last_name);
+}
+
+string	Contact::get_nickname(void) {
+	return (this->nickname);
+}
+
+string	Contact::get_phone_number(void) {
+	return (this->phone_number);
+}
+
+string	Contact::get_darkest_secret(void) {
+	return (this->darkest_secret);
+}
