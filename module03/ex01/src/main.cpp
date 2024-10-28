ddc5186225b94ec188e1e90dcd6f2f6ae12d2220
Boldizsar Szabo <szboldisz@gmail.com>
2024-10-28 14:59:59 +0100

    Update tests for module03/ex01

--- module03/ex01/src/main.cpp
+++ module03/ex01/src/main.cpp
@@ -6,58 +6,39 @@
 /*   By: bszabo <bszabo@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 10:47:38 by bszabo            #+#    #+#             */
-/*   Updated: 2024/10/25 13:12:36 by bszabo           ###   ########.fr       */
+/*   Updated: 2024/10/28 14:55:48 by bszabo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 
-// 'A' attacks 'B'
-static void attack(ScavTrap& A, ScavTrap& B) {
-    if (B.getHitPoints() < 80)
-        B.beRepaired(50);
+int main() {
+    // Creating a ClapTrap and a ScavTrap instance
+    ClapTrap clap("clap");
+    ScavTrap scav("scav");
 
-    A.attack(B.getName());
-    B.takeDamage(A.getAttackDamage());
+    std::cout << "\n--- Testing Constructor and Destruction Chain ---\n";
+    {
+        ScavTrap tempScav("TemporaryScavy");
+        tempScav.attack("Test Target");
+    } // This will show the destruction chain when tempScav goes out of scope
 
-    std::cout << std::endl;
-}
+    std::cout << "\n--- Testing ScavTrap Inherited and Overridden Functions ---\n";
+    
+    // Test overridden attack function from ScavTrap
+    scav.attack("Enemy");
 
-static void showInfo(ClapTrap& A, ClapTrap& B) {
-    std::cout << "----------" << std::endl;
-    std::cout << "Energy points left" << std::endl;
-    std::cout << A.getName() << " has " << A.getEnergyPoints() << std::endl;
-    std::cout << B.getName() << " has " << B.getEnergyPoints() << std::endl;
-    std::cout << "----------" << std::endl;
-    std::cout << "Hit points left" << std::endl;
-    std::cout << A.getName() << " has " << A.getHitPoints() << std::endl;
-    std::cout << B.getName() << " has " << B.getHitPoints() << std::endl;
-    std::cout << "----------" << std::endl;
-}
+    // Test if ClapTrap inherited functions work for ScavTrap
+    scav.takeDamage(15);
+    scav.beRepaired(10);
 
-int main(void) {
-    ScavTrap A("A");
-    std::cout << "----------" << std::endl;
-    ScavTrap B("B");
+    std::cout << "\n--- Testing ScavTrap Special Function ---\n";
+    scav.guardGate();
 
-    showInfo(A, B);
+    std::cout << "\n--- Final State Check ---\n";
+    std::cout << scav.getName() << " - Hit Points: " << scav.getHitPoints()
+              << ", Energy Points: " << scav.getEnergyPoints()
+              << ", Attack Damage: " << scav.getAttackDamage() << std::endl;
 
-    A.setAttackDamage(5);
-
-    // A attacks B
-    for (int i = 0; i < 6; i++) {
-        std::cout << "Attack #" << i + 1 << std::endl;
-        attack(A, B);
-    }
-
-    // B attacks A
-    for (int i = 0; i < 3; i++) {
-        std::cout << "Attack #" << i + 1 << std::endl;
-        attack(B, A);
-    }
-
-    A.guardGate();
-
-    showInfo(A, B);
+    return 0;
 }
